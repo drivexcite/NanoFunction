1e6661e6a7e14a78d1b00ac520de130ebe668843
Tona Diaz <drivexcite@gmail.com>
2019-08-26 11:20:20 -0600

    Adding deployment slot

--- build_infrastructure.ps1
+++ build_infrastructure.ps1
@@ -1,9 +1,9 @@
 $resourceGroup = 'DevOpsResourceGroup'
 $storageAccount = 'hwdevopsfuncstorage'
-$webAppPlan = 'DevOpsWebAppPlan'
 $functionApp = 'HwDevOpsFunctionApp'
 
 az group create --name $resourceGroup --location westus
 az storage account create --resource-group $resourceGroup --name $storageAccount --assign-identity --location westus --sku Standard_LRS
-az appservice plan create --resource-group $resourceGroup --name $webAppPlan --is-linux --location westus --sku B1
-az functionapp create --resource-group $resourceGroup --name $functionApp --plan $webAppPlan --os-type Linux  --runtime node --storage-account $storageAccount
+az functionapp create --consumption-plan-location westus --resource-group $resourceGroup --name $functionApp  --runtime node --storage-account $storageAccount
+az functionapp config appsettings set --resource-group $resourceGroup --name $functionApp --slot-settings "WEBSITE_RUN_FROM_PACKAGE=1"
+az functionapp deployment slot create --resource-group $resourceGroup --name $functionApp --slot staging
