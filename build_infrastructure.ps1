a06741bf2f2fc4a2148d87ca05c7a49c051d95a4
Tona Diaz <drivexcite@gmail.com>
2019-08-26 14:53:03 -0600

    Added KeyVault task to read secrets from a secure location.

--- build_infrastructure.ps1
+++ build_infrastructure.ps1
@@ -1,9 +1,21 @@
-$resourceGroup = 'DevOpsResourceGroup'
-$storageAccount = 'hwdevopsfuncstorage'
-$functionApp = 'HwDevOpsFunctionApp'
+$resourceGroup = "DevOpsResourceGroup"
+$storageAccount = "hwdevopsfuncstorage"
+$functionApp = "HwDevOpsFunctionApp"
+$keyVaultName = "HwDevOpsKeyVault"
+$servicePrincipalName = "9d25a2db-56c8-48f9-bdad-3f773fa22478"
 
 az group create --name $resourceGroup --location westus
 az storage account create --resource-group $resourceGroup --name $storageAccount --assign-identity --location westus --sku Standard_LRS
 az functionapp create --consumption-plan-location westus --resource-group $resourceGroup --name $functionApp  --runtime node --storage-account $storageAccount
 az functionapp config appsettings set --resource-group $resourceGroup --name $functionApp --slot-settings "WEBSITE_RUN_FROM_PACKAGE=1"
 az functionapp deployment slot create --resource-group $resourceGroup --name $functionApp --slot staging
+az keyvault create --resource-group $resourceGroup --name $keyVaultName --location westus --sku standard
+
+az keyvault secret set --vault-name $keyVaultName --name "Secret1" --value "SuperSecret1"
+az keyvault secret set --vault-name $keyVaultName --name "Secret2" --value "SuperSecret2"
+az keyvault secret set --vault-name $keyVaultName --name "Secret3" --value "SuperSecret3"
+az keyvault secret set --vault-name $keyVaultName --name "Secret4" --value "SuperSecret4"
+az keyvault secret set --vault-name $keyVaultName --name "Secret5" --value "SuperSecret5"
+
+#$servicePrincipalObjectId = az ad sp show --id $servicePrincipalName --query "objectId"
+az keyvault set-policy --resource-group $resourceGroup --name $keyVaultName --spn $servicePrincipalName --secret-permissions get list
--- src/SayHello/index.js
+++ src/SayHello/index.js
@@ -1,10 +1,16 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    let environmentVariables = '';
+
+    for(key in process.env) {
+        environmentVariables += `${key}=${process.env[key]};`;
+    }
+
     if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
+        context.res = {            
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name) + ' from 1.3'
+            body: "Hello " + (req.query.name || req.body.name) + " from 1.3. " + environmentVariables
         };
     }
     else {
